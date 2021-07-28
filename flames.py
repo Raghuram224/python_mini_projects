04e49f50f3d374f5c2ecc1f736209b2cb90e95dd
Raghuram <81945250+Raghuram224@users.noreply.github.com>
2021-07-28 08:08:50 -0700

    Update flames.py

--- flames.py
+++ flames.py
@@ -1,37 +1,38 @@
-player_name1="raghuram"
-player_name2=""
+name1=input("Enter your name:").lower()
+name2=input("Enter your partner name:").lower()
 
-name1=list(player_name1.lower())
-name2=list(player_name2.lower())
+name1=list(name1)
+name2=list(name2)
+name3=len(name1+name2)
+print(name3)
 
 for i in name1:
-    if i in name2:
-        
-        ind=name1.index(i)
-        name1[ind]="-"
-        ind=name2.index(i)
-        name2[ind]="-"
-        print("replaced")
+    for l in name2:
+        if i==l:
+            rep1=name1.index(i)
+            name1[rep1]="-"
+            rep2=name2.index(i)
+            name2[rep2]="-"
+    
 print(name1)
 print(name2)
-
-names=name1+name2
+name3=list(name1+name2)
 count=0
-for i in names:
-    if(i !="-" and i !=""):
-        count+=1
-
+for i in name3:
+    if (i !="-" and i !=""):
+        count +=1
 print(count)
-f=list("FLAMES")
-index=0
-
+f=["Friends","Love","Affection","Marriage","Enemy","Sibilings"]
 while len(f)>1:
-    for i in range (count):
-        index +=1
-        if index > len(f):
-            index=1
-    f.remove(f[index-1])
-    index -=1
-print(f)
+    c=count%len(f)
+    s_index=c-1
+    if s_index >=0:
+        left=f[: s_index]
+        right=f[s_index+1 :]
+        f=right+left
+    else:
+        f=f[:len(f)-1]
+print("reltionship is",f[0])
+        
 
 
